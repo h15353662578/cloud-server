74db86bfde1a31f946d2e9cd50e940c63a6b1961
华骏豪 <h1785222224@qq.com>
2020-12-19 23:28:28 +0800

    cloud

---
+++ cloud-sentinel-8200/src/main/java/com/hs/cloudsentinel/CloudSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.hs.cloudsentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSentinelApplication.class, args);
+    }
+
+}
---
+++ cloud-sentinel-8200/src/test/java/com/hs/cloudsentinel/CloudSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.cloudsentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
